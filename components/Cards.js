eea6e088afa0167c649c4a341d025362cf8b9314
Karem Elshendy <karemalshendy@gmail.com>
2021-10-19 02:26:48 +0200

    using fetch

--- components/Cards.js
+++ components/Cards.js
@@ -1,35 +1,44 @@
-import Axios from "axios";
-import React, { useEffect, useState } from "react";
-import Card from "./Card";
-import Page from "./Page";
+import Axios from "axios"
+import React, { useEffect, useState } from "react"
+import Card from "./Card"
+import Page from "./Page"
 
 function Cards() {
-  const [cards, setCard] = useState([]);
+  const [cards, setCard] = useState([])
 
   useEffect(() => {
-    async function fetchCards() {
-      try {
-        const response = await Axios.get("https://randomuser.me/api?results=6");
-        console.log(response.data.results);
-        setCard(response.data.results);
-      } catch (e) {
-        console.log("somethins went wrong!!");
-      }
-    }
-    fetchCards();
-  }, []);
+    // async function fetchCards() {
+    //   try {
+    //     const response = await Axios.get("https://randomuser.me/api?results=6")
+    //     console.log(response.data.results)
+    //     setCard(response.data.results)
+    //   } catch (e) {
+    //     console.log("somethins went wrong!!")
+    //   }
+    // }
+    // fetchCards()
+
+    fetch("https://randomuser.me/api?results=6")
+      .then((res) => {
+        return res.json()
+      })
+      .then((response) => {
+        console.log(response.results)
+        setCard(response.results)
+      }).catch(err=>{
+        console.log('Something went wrong!')
+      })
+  }, [])
 
   return (
     <Page title="CardsPage">
-      <section id="cards" >
-        <div className="container1">
-          {cards.map((card, index) => {
-            return <Card card={card} key={index} />;
-          })}
-        </div>
+      <section id="cards">
+        {cards.map((card, index) => {
+          return <Card card={card} key={index} />
+        })}
       </section>
     </Page>
-  );
+  )
 }
 
-export default Cards;
+export default Cards
